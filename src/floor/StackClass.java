2eb2cc1cb77bb99bd980886d4736a4440d86c756
kauramanp <33446281+kauramanp@users.noreply.github.com>
2022-11-19 23:45:43 +0530

    changes in the stack

--- src/floor/StackClass.java
+++ src/floor/StackClass.java
@@ -3,45 +3,54 @@ package floor;
 import java.util.Stack;
 
 public class StackClass {
+	Stack<Integer> tempList = new Stack<Integer>();
+	int lastCreatedFloor = -1;
 
 	public void setFloors(Stack<Integer> stack, int floors) {
-		Stack<Integer> tempList = new Stack<Integer>();
 		Boolean isBottomSet = false;
 		if (stack.isEmpty() == false) {
-			for (int i = 0; i < floors; i++) {
+			int i = 0;
+			while(stack.isEmpty() == false) {
 				System.out.println("Day : " + (i + 1));
-				int biggest = findBiggest(stack);
-				System.out.println("biggest " + biggest);
-				if (biggest == stack.elementAt(i)) {
-					System.out.print(biggest);
+			
+				if(stack.firstElement()  == floors && isBottomSet == false) {
 					isBottomSet = true;
+					tempList.push(stack.firstElement());
+					stack.remove(stack.firstElement());
 					if (tempList.isEmpty() == false) {
-						Stack<Integer> sorted = sortstack(tempList);
-						for(int k = 0; k<sorted.size(); k++) {
-							System.out.print(" "+sorted.elementAt(k));
-						}
+						printFloors(tempList, floors);
 						tempList.clear();
 					}
-				} else if (isBottomSet == true && stack.elementAt(i) < stack.elementAt(i - 1)) {
-					continue;
-				} else {
-					tempList.add(stack.elementAt(i));
+				}else  {
+					tempList.push(stack.firstElement());
+						if(stack.firstElement() == (lastCreatedFloor-1)) {
+							printFloors(tempList, floors);
+							tempList.clear();
+						}
+					stack.remove(stack.firstElement());
 				}
+				i++;
 			}
 		}
 	}
+	
+	public void printFloors(Stack<Integer> tempList, int floors) {
+		Stack<Integer> sorted = sortstack(tempList);
+		while(sorted.isEmpty() == false) {
+			 if((lastCreatedFloor != -1 && lastCreatedFloor ==  (sorted.lastElement() + 1)) || sorted.lastElement() == floors)
+			{
+				System.out.println(sorted.lastElement());
+				lastCreatedFloor = sorted.lastElement();
+				sorted.pop();
+				if(tempList.isEmpty() == false)
+					tempList.remove(lastCreatedFloor);
+			}else if(lastCreatedFloor !=  (sorted.lastElement() + 1)) {
+				break;
+			}
 
-	public int findBiggest(Stack<Integer> stack) {
-		int biggest = stack.firstElement();
-		for (int i = 0; i < stack.size(); i++) {
-			for (int j = i; j < stack.size(); j++)
-				if (biggest < stack.elementAt(j)) {
-					biggest = stack.elementAt(j);
-				}
 		}
-		return biggest;
-
 	}
+
 
 	public static Stack<Integer> sortstack(Stack<Integer> input) {
 		Stack<Integer> tmpStack = new Stack<Integer>();
